8b67f39139046606fc29b9bdf924b24a7740d868
Muriithi Gakuru <antony123muriithi@gmail.com>
2022-09-01 02:22:06 +0300

    complete assignment

---
+++ js/index.js
@@ -0,0 +1,45 @@
+async function fetchData() {
+    return fetch("https://api.github.com/search/users?q=octocat", {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/vnd.github.v3+json"
+        }
+    })
+        .then(res => res.json())
+}
+function filterData(login){
+    fetchData().then((data) => {
+        // console.log(data);
+        data.items.map(item => {
+            if (item.login === login){
+                return item;
+            }
+        })
+        
+    })
+}
+// filterData("marimeireles");
+// filterData("morehwachege");
+
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("github-form");
+    const search = document.getElementById("search");
+    let searchValue = search.value;
+    const user = document.getElementById("user-list");
+
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        console.log(search);
+        alert(searchValue)
+        // function display(){
+        //     let li = document.createElement("li");
+        //     li.innerHTML = `Name ${filterData(searchValue)}`;
+        //     user.appendChild(li);
+        // }
+        // display()
+
+
+        form.reset();
+    })
+})
